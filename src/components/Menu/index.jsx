8c3afe7463bca2f1bc5284a4fa18f1e8b4e9c994
jaovito <junior.vitorio.dias@gmail.com>
2020-07-25 14:14:05 -0300

    correction of useeffect

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import {Navbar, Nav} from 'react-bootstrap'
-// import { Container } from './styles';
 
 function Menu(props) {
   return (
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Card, Container } from './styles'
 import Menu from '../../components/Menu'
 
